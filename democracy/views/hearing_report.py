b386e2013d394960ba3e9f6c853222994cb0602a
SanttuA <santtualatalo@gmail.com>
2020-03-31 12:56:15 +0300

    reorganized comments and polls into their own section worksheets

--- democracy/views/hearing_report.py
+++ democracy/views/hearing_report.py
@@ -20,14 +20,12 @@ class HearingReport(object):
         self.hearing_worksheet = self.xlsdoc.add_worksheet('Hearing')
         self.hearing_worksheet.set_landscape()
         self.hearing_worksheet_active_row = 0
-        self.comments_worksheet = self.xlsdoc.add_worksheet('Comments')
-        self.comments_worksheet.set_landscape()
-        self.comments_worksheet_active_row = 0
-        self.polls_worksheet = self.xlsdoc.add_worksheet('Polls')
-        self.polls_worksheet.set_landscape()
-        self.polls_worksheet_active_row = 0
+        self.section_worksheet_active_row = 0
+
         self.format_bold = self.xlsdoc.add_format({'bold': True})
+        self.format_merge_title = self.xlsdoc.add_format({'align': 'center', 'underline': True})
         self.format_percent = self.xlsdoc.add_format({'num_format': '0 %'})
+
         self.context = context
 
     def add_hearing_row(self, label, content):
@@ -65,65 +63,95 @@ class HearingReport(object):
         self.add_hearing_row('Comments', str(self.json['n_comments']))
         self.add_hearing_row('Sections', str(len(self.json['sections'])))
 
-    def add_comment_row(self, commented_section, comment):
-        row = self.comments_worksheet_active_row
-        # add commented section
-        self.comments_worksheet.write(row, 0, commented_section)
-        # add author
-        self.comments_worksheet.write(row, 1, comment['author_name'])
-        # add creation date
-        self.comments_worksheet.write(row, 2, comment['created_at'])
-        # add votes
-        self.comments_worksheet.write(row, 3, comment['n_votes'])
-        # add label
-        self.comments_worksheet.write(row, 4, self._get_default_translation(comment['label'].get('label')
-                                                                            if comment['label'] else {}))
-        # add content
-        self.comments_worksheet.write(row, 5, comment['content'])
-        # add geojson
-        self.comments_worksheet.write(row, 6, json.dumps(comment['geojson']))
-        self.comments_worksheet.write(row, 7, ','.join(
-            image['url'] for image in comment['images']))
-        self.comments_worksheet_active_row += 1
+    def add_section_worksheet(self, section):
+        section_name = ""
+        if section['type'] == 'main':
+            section_name = section['type_name_singular']
+        else:
+            section_name = self._get_default_translation(section['title'])
 
-    def generate_comments_worksheet(self):
-        self.comments_worksheet.set_column('A:A', 25)
-        self.comments_worksheet.set_column('B:B', 20)
-        self.comments_worksheet.set_column('C:C', 20)
-        self.comments_worksheet.set_column('D:D', 5)
-        self.comments_worksheet.set_column('E:E', 20)
-        self.comments_worksheet.set_column('F:F', 200)
-        self.comments_worksheet.set_column('G:G', 100)
-        self.comments_worksheet.set_column('H:H', 100)
+        # worksheet name must be <= 31 chars
+        section_worksheet = self.xlsdoc.add_worksheet(section_name[:31])
+        section_worksheet.set_landscape()
+        section_worksheet.set_column('A:A', 50)
+        section_worksheet.set_column('B:B', 15)
+        section_worksheet.set_column('C:C', 10)
+        section_worksheet.set_column('D:D', 5)
+        section_worksheet.set_column('E:E', 50)
+        section_worksheet.set_column('F:F', 200)
+        section_worksheet.set_column('G:G', 100)
+        section_worksheet.set_column('H:H', 100)
 
-        self.comments_worksheet.set_header('Comments of %s' % self._get_default_translation(self.json['title']))
+        # add section title
+        self.section_worksheet_active_row = 0
+        section_worksheet.write(self.section_worksheet_active_row, 0, 'Section', self.format_bold)
+        self.section_worksheet_active_row += 1
+        section_worksheet.write(self.section_worksheet_active_row, 0, section_name)
+        self.section_worksheet_active_row += 2
 
-        self.comments_worksheet.write(0, 0, 'Section', self.format_bold)
-        self.comments_worksheet.write(0, 1, 'Author', self.format_bold)
-        self.comments_worksheet.write(0, 2, 'Created', self.format_bold)
-        self.comments_worksheet.write(0, 3, 'Votes', self.format_bold)
-        self.comments_worksheet.write(0, 4, 'Label', self.format_bold)
-        self.comments_worksheet.write(0, 5, 'Content', self.format_bold)
-        self.comments_worksheet.write(0, 6, 'Geojson', self.format_bold)
-        self.comments_worksheet.write(0, 7, 'Images', self.format_bold)
+        # add comments
+        self.add_section_comments(section, section_worksheet)
 
-        self.comments_worksheet_active_row = 1
+        # add some space between comments and polls
+        self.section_worksheet_active_row += 4
 
-        comments_count = 0
-
-        sections = [s for s in self.json['sections']]
-        for s in sections:
-            comments = [SectionCommentSerializer(c, context=self.context).data
-                        for c in SectionComment.objects.filter(section=s['id'])]
-            for comment in comments:
-                self.add_comment_row('%s: %s' % (s['type_name_singular'],
-                                                 self._get_default_translation(s['title'])), comment)
-                comments_count += 1
-
-        self.add_hearing_row('All comments', str(comments_count))
+        # add polls
+        self.add_section_polls(section, section_worksheet)
 
     
-    def generate_polls_worksheet(self):
+    def add_section_comments(self, section, section_worksheet):
+        '''
+        Author        | Created | Votes | Label   | Content        | Geojson      | Images
+        "commenter 1" | "date"  | num   | "label" | "comment text" | "geo data"   | "url"
+        "commenter 2" | "date"  | num   | "label" | "comment text" | "geo data"   | "url"
+        '''
+
+        # add comments title
+        row = self.section_worksheet_active_row
+        section_worksheet.merge_range(row, 0, row, 4, 'Comments', self.format_merge_title)
+        self.section_worksheet_active_row += 1
+
+        # add column headers
+        row = self.section_worksheet_active_row
+        section_worksheet.write(row, 0, 'Author', self.format_bold)
+        section_worksheet.write(row, 1, 'Created', self.format_bold)
+        section_worksheet.write(row, 2, 'Votes', self.format_bold)
+        section_worksheet.write(row, 3, 'Label', self.format_bold)
+        section_worksheet.write(row, 4, 'Content', self.format_bold)
+        section_worksheet.write(row, 5, 'Geojson', self.format_bold)
+        section_worksheet.write(row, 6, 'Images', self.format_bold)
+
+        self.section_worksheet_active_row += 1
+
+        # loop through comments in current section
+        comments = [SectionCommentSerializer(c, context=self.context).data
+                    for c in SectionComment.objects.filter(section=section['id'])]
+        for comment in comments:
+            self.add_comment_row(comment, section_worksheet)
+
+    def add_comment_row(self, comment, section_worksheet):
+        '''
+        "commenter 1" | "date"  | num   | "label" | "comment text" | "geo data"   | "url"
+        '''
+        row = self.section_worksheet_active_row
+        # add author
+        section_worksheet.write(row, 0, comment['author_name'])
+        # add creation date
+        section_worksheet.write(row, 1, comment['created_at'])
+        # add votes
+        section_worksheet.write(row, 2, comment['n_votes'])
+        # add label
+        section_worksheet.write(row, 3, self._get_default_translation(comment['label'].get('label')
+                                                                            if comment['label'] else {}))
+        # add content
+        section_worksheet.write(row, 4, comment['content'])
+        # add geojson
+        section_worksheet.write(row, 5, json.dumps(comment['geojson']))
+        section_worksheet.write(row, 6, ','.join(
+            image['url'] for image in comment['images']))
+        self.section_worksheet_active_row += 1
+
+    def add_section_polls(self, section, section_worksheet):
         '''
         Poll question | Poll type | Total votes | How many people answered the question
         "question?"   | "type"    | num         | num
@@ -132,40 +160,34 @@ class HearingReport(object):
         "2) option"   | 9         | 90%    
         -- empty rows after each question --
         '''
-        self.polls_worksheet.set_header('Polls of %s' % self._get_default_translation(self.json['title']))
-        self.polls_worksheet_active_row = 0
+        questions = section['questions']
 
-        self.polls_worksheet.set_column('A:A', 50)
-        self.polls_worksheet.set_column('B:B', 13)
-        self.polls_worksheet.set_column('C:C', 10)
-        self.polls_worksheet.set_column('D:D', 35)
-
-        # find sections with questions
-        sections = self.json['sections']
-        questions = []
-        for section in sections:
-            questions.extend(section['questions'])
+        # add polls title if polls exist
+        if len(questions) > 0:
+            row = self.section_worksheet_active_row
+            section_worksheet.merge_range(row, 0, row, 4, 'Polls', self.format_merge_title)
+            self.section_worksheet_active_row += 1
 
         # add question data for each question
         for question in questions:
-            self.add_poll_question_rows(question)
+            self.add_poll_question_rows(question, section_worksheet)
             # add space between questions
-            self.polls_worksheet_active_row += 2
+            self.section_worksheet_active_row += 2
 
 
-    def add_poll_question_rows(self, question):
+    def add_poll_question_rows(self, question, section_worksheet):
         '''
         Poll question | Poll type | Total votes | how many people answered the question
         "question?"   | "type"    | num         | num
         '''
-        row = self.polls_worksheet_active_row
+        row = self.section_worksheet_active_row
         chart_location = (row, 5) # set chart to start on the same row as headers
         # headers
-        self.polls_worksheet.write(row, 0, 'Poll question', self.format_bold)
-        self.polls_worksheet.write(row, 1, 'Poll type', self.format_bold)
-        self.polls_worksheet.write(row, 2, 'Total votes', self.format_bold)
-        self.polls_worksheet.write(row, 3, 'How many people answered the question', self.format_bold)
-        self.polls_worksheet_active_row += 1
+        section_worksheet.write(row, 0, 'Poll question', self.format_bold)
+        section_worksheet.write(row, 1, 'Poll type', self.format_bold)
+        section_worksheet.write(row, 2, 'Total votes', self.format_bold)
+        section_worksheet.write(row, 3, 'How many people answered the question', self.format_bold)
+        self.section_worksheet_active_row += 1
 
         # options total vote count
         options = question['options']
@@ -174,53 +196,55 @@ class HearingReport(object):
             total_options_answers += option['n_answers']
 
         # values under headers
-        row = self.polls_worksheet_active_row
+        row = self.section_worksheet_active_row
         question_text = self._get_default_translation(question['text'])
 
-        self.polls_worksheet.write(row, 0, question_text)
-        self.polls_worksheet.write(row, 1, question['type'])
-        self.polls_worksheet.write(row, 2, total_options_answers)
-        self.polls_worksheet.write(row, 3, question['n_answers'])
+        section_worksheet.write(row, 0, question_text)
+        section_worksheet.write(row, 1, question['type'])
+        section_worksheet.write(row, 2, total_options_answers)
+        section_worksheet.write(row, 3, question['n_answers'])
         # store n_answers cell location for option answer % calculation
         total_answers_cell = xl_rowcol_to_cell(row, 2)
-        self.polls_worksheet_active_row += 1
+        self.section_worksheet_active_row += 1
 
         # add option rows, store option cell info
-        option_cells = self.add_poll_question_option_rows(options, total_answers_cell)
+        option_cells = self.add_poll_question_option_rows(options, total_answers_cell,
+            section_worksheet)
 
         # add space after options to make room for chart (2 rows per option)
         empty_rows_after_options = len(options) * 2
-        self.polls_worksheet_active_row += empty_rows_after_options
+        self.section_worksheet_active_row += empty_rows_after_options
         # add chart
-        self.add_poll_question_chart(question_text, option_cells, chart_location, empty_rows_after_options)
+        self.add_poll_question_chart(question_text, option_cells, chart_location,
+            section_worksheet, empty_rows_after_options)
 
     
-    def add_poll_question_option_rows(self, options, total_answers_cell):
+    def add_poll_question_option_rows(self, options, total_answers_cell, section_worksheet):
         '''
         Options     | Votes | Votes % 
         "1) option" | 1     | 10 %    
         '''
-        row = self.polls_worksheet_active_row
+        row = self.section_worksheet_active_row
         # headers
-        self.polls_worksheet.write(row, 0, 'Options', self.format_bold)
-        self.polls_worksheet.write(row, 1, 'Votes', self.format_bold)
-        self.polls_worksheet.write(row, 2, 'Votes %', self.format_bold)
-        self.polls_worksheet_active_row += 1
+        section_worksheet.write(row, 0, 'Options', self.format_bold)
+        section_worksheet.write(row, 1, 'Votes', self.format_bold)
+        section_worksheet.write(row, 2, 'Votes %', self.format_bold)
+        self.section_worksheet_active_row += 1
 
         # store category and value start locations for later calculations
-        categories_start = (self.polls_worksheet_active_row, 0)
-        values_start = (self.polls_worksheet_active_row, 2)
+        categories_start = (self.section_worksheet_active_row, 0)
+        values_start = (self.section_worksheet_active_row, 2)
         # values under headers
         for index, option in enumerate(options, start=1):
-            row = self.polls_worksheet_active_row
-            self.polls_worksheet.write(row, 0, f"{index}) {self._get_default_translation(option['text'])}")
-            self.polls_worksheet.write(row, 1, option['n_answers'])
-            self.polls_worksheet.write(row, 2, f"={xl_rowcol_to_cell(row, 1)}/{total_answers_cell}", self.format_percent)
-            self.polls_worksheet_active_row += 1
+            row = self.section_worksheet_active_row
+            section_worksheet.write(row, 0, f"{index}) {self._get_default_translation(option['text'])}")
+            section_worksheet.write(row, 1, option['n_answers'])
+            section_worksheet.write(row, 2, f"={xl_rowcol_to_cell(row, 1)}/{total_answers_cell}", self.format_percent)
+            self.section_worksheet_active_row += 1
 
         # store category and value end locations for later calculations
-        categories_end = (self.polls_worksheet_active_row-1, 0) # -1 row to not include empty row
-        values_end = (self.polls_worksheet_active_row-1, 2) # -1 row to not include empty row
+        categories_end = (self.section_worksheet_active_row-1, 0) # -1 row to not include empty row
+        values_end = (self.section_worksheet_active_row-1, 2) # -1 row to not include empty row
 
         # return dict containing option cell info
         return {
@@ -231,14 +255,16 @@ class HearingReport(object):
             'option_count': len(options)
             }
     
-    def add_poll_question_chart(self, question_text, option_cells, chart_location, empty_rows_after_options = 0):        
+    def add_poll_question_chart(self, question_text, option_cells, chart_location,
+        section_worksheet, empty_rows_after_options = 0, ):        
         chart = self.xlsdoc.add_chart({'type': 'bar'})
         # Configure the series.
         # [sheetname, first_row, first_col, last_row, last_col]
+        section_name = section_worksheet.get_name()
         chart.add_series({
-            'categories': ['Polls', option_cells['categories_start'][0], option_cells['categories_start'][1],
+            'categories': [section_name, option_cells['categories_start'][0], option_cells['categories_start'][1],
                 option_cells['categories_end'][0], option_cells['categories_end'][1]], 
-            'values':     ['Polls', option_cells['values_start'][0], option_cells['values_start'][1],
+            'values':     [section_name, option_cells['values_start'][0], option_cells['values_start'][1],
                 option_cells['values_end'][0], option_cells['values_end'][1]],
         })
 
@@ -247,10 +273,9 @@ class HearingReport(object):
             'name': question_text,
             'name_font': {'name': 'Calibri', 'size': 14, 'bold': False}
         }) # poll question
-        chart.set_legend({'none': True}) # removes "series 1"
+        chart.set_legend({'none': True}) # removes "series 1" chart title
         
         # chart and axis styles
-        #chart.set_style(1)
         chart.set_x_axis({
             'max': 1, # percent scale to always be up to 100%
             'num_font': {'name': 'Calibri', 'size': 9},
@@ -267,13 +292,16 @@ class HearingReport(object):
         chart.set_size({'width': 480, 'height': chart_height})
 
         # Insert the chart into the worksheet.
-        self.polls_worksheet.insert_chart(chart_location[0], chart_location[1], chart)
+        section_worksheet.insert_chart(chart_location[0], chart_location[1], chart)
 
 
     def get_xlsx(self):
         self.generate_hearing_worksheet()
-        self.generate_comments_worksheet()
-        self.generate_polls_worksheet()
+
+        sections = self.json['sections']
+        for section in sections:
+            self.add_section_worksheet(section)
+
         self.xlsdoc.close()
 
         return self.buffer.getvalue()
